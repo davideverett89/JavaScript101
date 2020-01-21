028b36d0740359b67d45293f661befb6b81b31e3
David Everett <davideverett1989@gmail.com>
2020-01-20 18:48:00 -0600

    Added stuff.

---
+++ functions.js
@@ -0,0 +1,6 @@
+const nuggetizer = (animal) => {
+    return `${animal} nuggets`;
+}
+
+console.log(nuggetizer("chicken"));
+console.log(nuggetizer("fish"));
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 console.log("JavaScript101");
 
-function whatTheFuck () {
-    console.log("What the fuck!");
+function nuggetizer() {
+    
 }
-whatTheFuck();
