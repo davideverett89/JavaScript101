b45eb8253b2dd6d8fa6f9954a0e1541482fff8ee
David Everett <davideverett1989@gmail.com>
2020-01-21 18:47:51 -0600

    Exercises done.

--- combo.js
+++ combo.js
@@ -1,22 +1,44 @@
-console.log("Combo");
+// const evenOdd = (num) => {
+//     return num % 2 === 0 ? "Even" : "Odd";
+// }
 
-const evenOdd = (num) => {
-    return num % 2 === 0 ? "Even" : "Odd";
-}
+// const opposite = (num) => {
+//     return -num;
+// }
 
-const opposite = (num) => {
-    return -num;
-}
+// const palindrome = (str) => {
+//     let arr = [];
+//     let lowerCase = str.toLowerCase();
+//     let strSplit = lowerCase.split('');
+//     let filteredArray = strSplit.filter(function(index) {
+//       if (index.match(/^[A-Za-z]+$/)) {
+//         return index;
+//       }
+//       else if (index.match(/^[0-9][A-Za-z0-9 -]*$/)) {
+//         return index;
+//       }
+//     });
+//     for (let i = filteredArray.length - 1; i >= 0; i--) {
+//       arr.push(filteredArray[i]);
+//     }
 
-const palindrome = (str) => {
-    return str.toLowerCase().split("").reverse().join("") === str ? true : false;
-}
+//     if (filteredArray.toString() === arr.toString()) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//    };
+
+
+// const palindrome = (str) => {
+//     return str.toLowerCase().split("").reverse().join("") === str ? true : false;
+// }
 
 const lawrence = (str) => {
-    return str === "alligator" ? "small" : "wide";
+    return str.toLowerCase() === "alligator" ? "small" : "wide";
 }
 
-console.log(evenOdd(5));
-console.log(opposite(-10));
-console.log(palindrome("racecard"));
-console.log(lawrence("alligator"));
+// console.log(evenOdd(5));
+// console.log(opposite(-10));
+// console.log(palindrome("able !was i ere i. saw elba"));
+console.log(lawrence("Alligator"));
